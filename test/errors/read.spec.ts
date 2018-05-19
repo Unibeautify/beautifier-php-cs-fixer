131f9ea77a146577a40d41359c90daa4d1b3fe15
Steven Zeck <steven.zeck@gmail.com>
2018-05-19 14:40:32 -0500

    Add more test coverage

---
+++ test/errors/read.spec.ts
@@ -0,0 +1,20 @@
+import { newUnibeautify, Beautifier } from "unibeautify";
+import beautifier from "../../src";
+import { raw } from "../utils";
+// tslint:disable:mocha-no-side-effect-code
+const fs = require("fs");
+fs.readFile = jest.fn((filepath, cb) => cb(new Error("Read file failed")));
+test(`should error writing file`, () => {
+  const text: string = `<?php $temp = 1; ?>`;
+  const unibeautify = newUnibeautify();
+  unibeautify.loadBeautifier(beautifier);
+  return expect(
+    unibeautify.beautify({
+      languageName: "PHP",
+      options: {
+        PHP: {},
+      },
+      text,
+    })
+  ).rejects.toThrowError("Read file failed");
+});
---
+++ test/errors/write.spec.ts
@@ -0,0 +1,23 @@
+import { newUnibeautify, Beautifier } from "unibeautify";
+import beautifier from "../../src";
+import { raw } from "../utils";
+// tslint:disable:mocha-no-side-effect-code
+const fs = require("fs");
+fs.writeFile = jest.fn((filepath, content, cb) =>
+  cb(new Error("Write file failed"))
+);
+test(`should error writing file`, () => {
+  const text: string = `<?php $temp = 1; ?>`;
+  const unibeautify = newUnibeautify();
+  unibeautify.loadBeautifier(beautifier);
+  return expect(
+    unibeautify
+    .beautify({
+      languageName: "PHP",
+      options: {
+        PHP: {},
+      },
+      text,
+    })
+  ).rejects.toThrowError("Write file failed");
+});
